c1c57c1ab6192236167b9b1ac676d5b5e1386404
Diego Almeida <dyhalmeida@gmail.com>
2021-08-24 15:15:43 -0300

    Implementação do ciclo de vida (OnInit)
    Chamado depois que Angular inicializou todas as propriedades vinculadas
    a dados de uma diretiva. A definição do método ngOnInit () serve para lidar
    com quaisquer tarefas de inicialização adicionais.

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,9 +1,35 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ICourse } from './course';
 
 @Component({
     selector: 'app-course-list',
     templateUrl: 'course-list.component.html',
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
+
+    courses: ICourse[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular Forms',
+                imageUrl: '',
+                price: 99.99,
+                code: 'ABC-112345',
+                duration: 120,
+                rating: 5.4            
+            },
+            {
+                id: 1,
+                name: 'Angular HTTP',
+                imageUrl: '',
+                price: 45.99,
+                code: 'ABC-112346',
+                duration: 80,
+                rating: 4            
+            }
+        ]
+    }
 
 }
