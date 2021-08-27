eafa885dfa42958ad044343852f1043cec5ad645
Diego Almeida <dyhalmeida@gmail.com>
2021-08-27 14:27:22 -0300

    Adicionado o módulo de rotas e roteamento
    Foi utilizado o módulo de rotas para roteamento dos componentes.
    Além da rota de 'courses', foi utilizada também a rota raiz '' com
    redirecionamento para 'courses' e também a rota '**' not-found.
    Obs: Quando um componente é exibido através de rotas não é necessário
    o uso do selector no decorator @Component

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -3,7 +3,6 @@ import { ICourse } from './course';
 import { CourseService } from "./courses.service";
 
 @Component({
-    selector: 'app-course-list',
     templateUrl: 'course-list.component.html',
 })
 export class CourseListComponent implements OnInit {
---
+++ src/app/not-found/not-found.component.ts
@@ -0,0 +1,6 @@
+import { Component } from "@angular/core";
+
+@Component({
+    template: '<h2>Erro 404 - Página não encontrada</h2>'
+})
+export class NotFoundComponent {}
